abc36ebb5797b2b0591dcccf9a3a219d62d67f5c
ŃaSrO <bouaouina.nasreddine@gmail.com>
2021-04-05 16:03:40 +0200

    Google Login using Passport

--- app.js
+++ app.js
@@ -3,9 +3,13 @@ const dotenv = require("dotenv");
 const morgan = require("morgan");
 const exphbs = require("express-handlebars");
 const path = require("path");
-
+const passport = require("passport");
+const session = require("express-session");
 // Load config
 dotenv.config({ path: "./config/config.env" });
+
+// Passport config
+require("./config/passport")(passport);
 
 //  Connect to Database
 const connectDB = require("./config/db");
@@ -22,11 +26,25 @@ if (process.env.NODE_ENV === "development") {
 app.engine(".hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
 app.set("view engine", ".hbs");
 
+// Sessions
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
 // Static Folder
 app.use(express.static(path.join(__dirname, "public")));
 
 // Routes
 app.use("/", require("./routes/index"));
+app.use("/auth", require("./routes/auth"));
 
 const PORT = process.env.PORT || 3000;
 app.listen(
