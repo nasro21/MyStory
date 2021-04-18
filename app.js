fb24ddb17d8f684f91c6fe150a5f18f8dbc34cd2
ŃaSrO <bouaouina.nasreddine@gmail.com>
2021-04-18 16:14:35 +0200

    Add story & show stories

--- app.js
+++ app.js
@@ -20,13 +20,31 @@ connectDB();
 
 const app = express();
 
+// Body parser
+app.use(express.urlencoded({ extenden: false }));
+app.use(express.json());
+
 // Logging with MORGAN
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
+// Handlebars Helpers
+const { formatDate, stripTags, truncate } = require("./helpers/hbs");
+
 // Hndlebars
-app.engine(".hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
+app.engine(
+  ".hbs",
+  exphbs({
+    helpers: {
+      formatDate,
+      stripTags,
+      truncate,
+    },
+    defaultLayout: "main",
+    extname: ".hbs",
+  })
+);
 app.set("view engine", ".hbs");
 
 // Sessions
@@ -50,6 +68,7 @@ app.use(express.static(path.join(__dirname, "public")));
 // Routes
 app.use("/", require("./routes/index"));
 app.use("/auth", require("./routes/auth"));
+app.use("/stories", require("./routes/stories"));
 
 const PORT = process.env.PORT || 3000;
 app.listen(
